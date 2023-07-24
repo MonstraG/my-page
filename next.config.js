200dbb88cd726f6c6996a12a707a953add76aecb
Arseny Garelyshev <monstrag@gmail.com>
2023-07-24 21:33:07 +0200

    chore: redefine imageSizes/deviceSizes to serve correct image sizes

--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ const nextConfig = {
 			}
 		];
 	},
+	images: {
+		imageSizes: [32, 48, 64, 80, 96, 128, 256, 384],
+		deviceSizes: [640, 828, 1080, 1536, 1920, 2048]
+	},
 	eslint: {
 		dirs: ["src"]
 	}
