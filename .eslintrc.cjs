34c6f84d71f601d4f2c46cecf8308ec1a9cdfedf
Arseny Garelyshev <monstrag@gmail.com>
2023-07-26 20:30:37 +0200

    fix: fix linter failing

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,7 +22,7 @@ const config = {
 		"@typescript-eslint/no-inferrable-types": "off",
 		"@typescript-eslint/consistent-type-definitions": "off"
 	},
-	ignorePatterns: [".eslintrc.mjs", "next.config.mjs"]
+	ignorePatterns: [".eslintrc.cjs", "next.config.mjs"]
 };
 
-export default config;
+module.exports = config;
