d575e104c7fde30197fc59a4d5d05929262ebdf1
Arseny Garelyshev <monstrag@gmail.com>
2023-07-28 22:38:19 +0200

    feat: log rate limit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,7 +17,6 @@ const config = {
 	root: true,
 	rules: {
 		"react-hooks/exhaustive-deps": "error",
-		"no-console": ["error", { allow: ["warn", "error"] }],
 		"@typescript-eslint/no-empty-interface": ["error", { allowSingleExtends: true }],
 		"@typescript-eslint/no-unused-vars": ["error", { ignoreRestSiblings: true }],
 		"@typescript-eslint/no-inferrable-types": "off", // I want to be explicit
