0c25026db487d9fa88785cf369b15a7ed9fd947e
Arseny Garelyshev <monstrag@gmail.com>
2023-07-28 19:18:02 +0200

    chore: remove unneeded settings from nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,9 @@ import withBundleAnalyzer from "@next/bundle-analyzer";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	optimizeFonts: false,
 	images: {
 		imageSizes: [32, 48, 64, 80, 96, 128, 256, 384],
 		deviceSizes: [640, 828, 1080, 1536, 1920, 2048]
-	},
-	eslint: {
-		dirs: ["src"]
 	}
 };
 
