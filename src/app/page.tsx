9ddb19483bb7454908c91db2cb46d5e5c7652fcb
Arseny Garelyshev <monstrag@gmail.com>
2023-07-28 23:06:49 +0200

    feat: WIP add fetched profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@ import type { NextPage } from "next";
 import styles from "./page.module.scss";
 import { Open_Sans } from "next/font/google";
 import { Contributions } from "@/components/Contributions/Contributions";
+import { Profile } from "@/components/Profile/Profile";
 
 const openSans = Open_Sans({ subsets: ["latin"] });
 
 const IndexPage: NextPage = () => (
 	<main className={openSans.className}>
 		<div className={styles.container}>
-			<h1>Arseny Garelyshev</h1>
+			<Profile />
 			<Contributions />
 		</div>
 	</main>
