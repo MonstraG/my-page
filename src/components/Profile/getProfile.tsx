a770a07c38ffa3dec5ae5817e3150b5dd01e6eda
Arseny Garelyshev <monstrag@gmail.com>
2023-08-21 21:42:17 +0200

    fix: fix requests not caching by merging it into one

--- src/components/Profile/getProfile.tsx
+++ src/components/Profile/getProfile.tsx
@@ -1,38 +1,11 @@
 import { fetchGithub } from "@/components/fetchGithub";
 
-const query = `
-query Profile($userName: String!) {
-  user(login: $userName) {
-    avatarUrl(size: 48)
-    company
-    url
-    name
-  }
-}`;
-
-const variables = `
-  {
-    "userName": "MonstraG"
-  }
-`;
-
-const body = {
-	query,
-	variables
-};
-
-interface GithubProfile {
-	data: {
-		user: Profile;
-	};
-}
-
-export interface Profile {
+export interface ProfileData {
 	avatarUrl: string;
 	company: string;
 	url: string;
 	name: string;
 }
 
-export const getProfile = async (): Promise<Profile> =>
-	fetchGithub<GithubProfile>(body).then((data) => data.data.user);
+export const getProfile = async (): Promise<ProfileData> =>
+	await fetchGithub().then((data) => data.data.user);
