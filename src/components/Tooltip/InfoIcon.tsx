1f17a6af63387566fe7e71d8c69dc23533547e82
ArsenyGarelyshev <monstrag@gmail.com>
2023-08-22 23:58:25 +0200

    feat: add WIP info icon

---
+++ src/components/Tooltip/InfoIcon.tsx
@@ -0,0 +1,6 @@
+import type { FC } from "react";
+import styles from "@/components/Tooltip/InfoIcon.module.scss";
+
+export const InfoIcon: FC = () => {
+	return <div className={styles.icon}>i</div>;
+};
