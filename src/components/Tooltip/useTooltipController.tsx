3276d245512a169285bcddae2fc405357e1d4a69
Arseny Garelyshev <monstrag@gmail.com>
2023-08-23 21:18:50 +0200

    feat: add tooltip animations

--- src/components/Tooltip/useTooltipController.tsx
+++ src/components/Tooltip/useTooltipController.tsx
@@ -1,9 +1,13 @@
-import { type CSSProperties, useCallback, useMemo, useState } from "react";
+import { type CSSProperties, useCallback, useMemo, useRef, useState } from "react";
 
 interface TooltipState<T> {
 	anchor: HTMLElement | null;
 	context: T | null;
+	open: boolean;
+	opacity: 0 | 1;
 }
+
+const tooltipSpeed = 100;
 
 export interface TooltipControls<T> {
 	open: (anchor: HTMLElement, context: T) => void;
@@ -19,8 +23,12 @@ export interface TooltipController<T> {
 export const useTooltipController = <T,>(): TooltipController<T> => {
 	const [tooltipState, setTooltipState] = useState<TooltipState<T>>({
 		anchor: null,
-		context: null
+		context: null,
+		open: false,
+		opacity: 0
 	});
+
+	const timeout = useRef<ReturnType<typeof setTimeout> | null>(null);
 
 	const position: CSSProperties = (() => {
 		if (!tooltipState.anchor) {
@@ -29,16 +37,32 @@ export const useTooltipController = <T,>(): TooltipController<T> => {
 		return {
 			left: tooltipState.anchor.offsetLeft,
 			top: tooltipState.anchor.offsetTop,
-			display: "block"
+			display: tooltipState.open ? "block" : "none",
+			opacity: tooltipState.opacity
 		};
 	})();
 
 	const open = useCallback((anchor: HTMLElement, context: T) => {
-		setTooltipState({ anchor, context });
+		setTooltipState({ anchor, context, open: true, opacity: 0 });
+
+		if (timeout.current) {
+			clearTimeout(timeout.current);
+		}
+
+		timeout.current = setTimeout(() => {
+			setTooltipState((prev) => ({ ...prev, opacity: 1 }));
+		}, tooltipSpeed);
 	}, []);
 
 	const close = useCallback(() => {
-		setTooltipState((p) => ({ anchor: null, context: p.context }));
+		setTooltipState((prev) => ({ ...prev, opacity: 0 }));
+
+		if (timeout.current) {
+			clearTimeout(timeout.current);
+		}
+		timeout.current = setTimeout(() => {
+			setTooltipState((prev) => ({ ...prev, open: false, opacity: 0 }));
+		}, tooltipSpeed);
 	}, []);
 
 	return {
