352ba3c346552d351dd48bd613eef55e774d8ad4
Arseny Garelyshev <monstrag@gmail.com>
2023-08-30 18:18:42 +0200

    feat: move dice into a separate page

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,13 @@
+import { Open_Sans } from "next/font/google";
+import type { FCC } from "@/types/react";
+import styles from "@/app/(app)/page.module.scss";
+
+const openSans = Open_Sans({ subsets: ["latin"] });
+
+const AppLayout: FCC = ({ children }) => (
+	<main className={openSans.className}>
+		<div className={styles.container}>{children}</div>
+	</main>
+);
+
+export default AppLayout;
