9b25ea814dfa80e9317a32a6079f07f672d327d2
Arseny Garelyshev <monstrag@gmail.com>
2023-09-08 22:19:14 +0200

    fix: keys

--- src/components/DiceRoll/DiceBag.tsx
+++ src/components/DiceRoll/DiceBag.tsx
@@ -22,13 +22,13 @@ export const DiceBag: FC<Props> = ({ title, dice, onDiceClick }) => (
 	<div className={styles.diceSet}>
 		<h2>{title}</h2>
 		<div className={styles.diceButtons}>
-			{groupSame(dice).map((sameSideDice, index) => (
-				<div key={index}>
-					{sameSideDice.map((die) => (
+			{groupSame(dice).map((sameSideDice, groupIndex) => (
+				<div key={groupIndex}>
+					{sameSideDice.map((die, dieIndex) => (
 						<button
-							key={index}
+							key={dieIndex}
 							onClick={() => {
-								onDiceClick(die, index);
+								onDiceClick(die, dieIndex);
 							}}
 							className={styles.dieButton}
 						>
