66bf0a30b0f4c63928d4daf810a3f748086fbce1
Arseny Garelyshev <monstrag@gmail.com>
2023-09-08 21:14:32 +0200

    feat: group dice of same size together

--- src/components/DiceRoll/DiceBag.tsx
+++ src/components/DiceRoll/DiceBag.tsx
@@ -1,6 +1,17 @@
 import { FC } from "react";
 import styles from "@/components/DiceRoll/DiceRoll.module.scss";
 
+function groupSame(array: number[]): number[][] {
+	return array.reduce<number[][]>((acc, val) => {
+		if (!acc.length || acc[acc.length - 1][0] !== val) {
+			acc.push([val]);
+		} else {
+			acc[acc.length - 1].push(val);
+		}
+		return acc;
+	}, []);
+}
+
 interface Props {
 	title: string;
 	dice: number[];
@@ -11,16 +22,20 @@ export const DiceBag: FC<Props> = ({ title, dice, onDiceClick }) => (
 	<div className={styles.diceSet}>
 		<h2>{title}</h2>
 		<div className={styles.diceButtons}>
-			{dice.map((die, index) => (
-				<button
-					key={index}
-					onClick={() => {
-						onDiceClick(die, index);
-					}}
-					className={styles.dieButton}
-				>
-					d{die}
-				</button>
+			{groupSame(dice).map((sameSideDice, index) => (
+				<div key={index}>
+					{sameSideDice.map((die) => (
+						<button
+							key={index}
+							onClick={() => {
+								onDiceClick(die, index);
+							}}
+							className={styles.dieButton}
+						>
+							d{die}
+						</button>
+					))}
+				</div>
 			))}
 		</div>
 	</div>
