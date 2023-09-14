1684b1bb053a5dd583b71780ed83139825e6ea8b
Arseny Garelyshev <monstrag@gmail.com>
2023-09-14 22:36:28 +0200

    fix: initialize editor correctly

--- src/app/(app)/write/Editable.tsx
+++ src/app/(app)/write/Editable.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useId, useRef } from "react";
+import { FC, useEffect, useRef } from "react";
 import EditorJS, { OutputData } from "@editorjs/editorjs";
 
 interface Props {
@@ -6,36 +6,36 @@ interface Props {
 }
 
 export const Editable: FC<Props> = () => {
-	const id = useId();
-
-	const editor = useRef<EditorJS>(
-		new EditorJS({
-			holder: id,
-			autofocus: true,
-			placeholder: "Start writing here",
-			tools: {
-				// ... your tools
-			},
-			onReady: function () {
-				console.log("onReady");
-				console.log(editor.current);
-			},
-			onChange: function (api, event) {
-				console.log("something changed", event);
-			}
-		})
-	);
+	const editorContainer = useRef<HTMLDivElement | null>(null); // create a ref to hold the div reference
+	const editorInstance = useRef<EditorJS | null>(null); // an instance to hold EditorJs instance
 
 	useEffect(() => {
-		const reference = editor.current;
+		if (editorContainer.current && !editorInstance.current) {
+			editorInstance.current = new EditorJS({
+				holder: editorContainer.current,
+				autofocus: true,
+				placeholder: "Start writing here",
+				tools: {
+					// ... your tools
+				},
+				onReady: function () {
+					console.log("onReady");
+					console.log(editorInstance.current);
+				},
+				onChange: function (api, event) {
+					console.log("something changed", event);
+				}
+			});
+		}
+
 		return () => {
-			// types lie, initially editor has only `isReady`.
-			// eslint-disable-next-line
-			if (reference.destroy) {
-				reference.destroy();
+			// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+			if (editorInstance.current?.destroy) {
+				editorInstance.current.destroy();
+				editorInstance.current = null;
 			}
 		};
 	}, []);
 
-	return <div id={id} />;
+	return <div ref={editorContainer} />;
 };
