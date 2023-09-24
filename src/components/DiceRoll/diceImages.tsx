d33c718950e8f1a45824752f1e03aa3297529fc4
Arseny Garelyshev <monstrag@gmail.com>
2023-09-24 22:36:18 +0200

    feat(dice): add colors

--- src/components/DiceRoll/diceImages.tsx
+++ src/components/DiceRoll/diceImages.tsx
@@ -1,4 +1,5 @@
-import { FC, ReactNode, SVGProps } from "react";
+import type { FC, ReactNode, SVGProps } from "react";
+import styles from "@/components/DiceRoll/diceImages.module.scss";
 
 const DieBaseSvg: FC<SVGProps<SVGSVGElement>> = (props) => (
 	<svg width={adjustImageWidth(40, 40)} height="40" viewBox="0 0 40 40" {...props} />
@@ -14,43 +15,43 @@ function adjustImageWidth(width: number, height: number) {
 }
 
 const D2: FC = () => (
-	<DieBaseSvg>
+	<DieBaseSvg className={styles.d2}>
 		<circle cx="20" cy="20" r="20" />
 	</DieBaseSvg>
 );
 
 const D4: FC = () => (
-	<DieBaseSvg>
+	<DieBaseSvg className={styles.d4}>
 		<path d="M40 37.4H0L20 2.7z" />
 	</DieBaseSvg>
 );
 
 const D6: FC = () => (
-	<DieBaseSvg>
+	<DieBaseSvg className={styles.d6}>
 		<path d="M0 0h40v40H0z" />
 	</DieBaseSvg>
 );
 
 const D8: FC = () => (
-	<DieBaseSvg>
+	<DieBaseSvg className={styles.d8}>
 		<path d="M20 40L0 20 20 0l20 20z" />
 	</DieBaseSvg>
 );
 
 const D10: FC = () => (
-	<DieBaseSvg>
+	<DieBaseSvg className={styles.d10}>
 		<path d="M20 0L0 16.9v6.3L20 40l20-16.9V17z" />
 	</DieBaseSvg>
 );
 
 const D12: FC = () => (
-	<DieBaseSvg>
+	<DieBaseSvg className={styles.d12}>
 		<path d="M20 40L0 24.8 7.6 0h24.7L40 24.8z" />
 	</DieBaseSvg>
 );
 
 const D20: FC = () => (
-	<DieBaseSvg>
+	<DieBaseSvg className={styles.d20}>
 		<path d="M2.7 30V10L20 0l17.3 10v20L20 40z" />
 	</DieBaseSvg>
 );
