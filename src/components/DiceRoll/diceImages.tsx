0bc04020bf6d5baedc38ae2007021ab896cd4789
Arseny Garelyshev <monstrag@gmail.com>
2023-10-09 20:10:56 +0200

    feat: add numbers to dice faces

--- src/components/DiceRoll/diceImages.tsx
+++ src/components/DiceRoll/diceImages.tsx
@@ -1,8 +1,12 @@
 import type { FC, ReactNode, SVGProps } from "react";
 import styles from "@/components/DiceRoll/diceImages.module.scss";
 
-const DieBaseSvg: FC<SVGProps<SVGSVGElement>> = (props) => (
-	<svg width={adjustImageWidth(40, 40)} height="40" viewBox="0 0 40 40" {...props} />
+const DieSvg: FC<SVGProps<SVGSVGElement>> = (props) => (
+	<svg width={adjustImageWidth(40, 40)} height="40" viewBox="-1 -1 42 42" {...props} />
+);
+
+const DieText: FC<SVGProps<SVGTextElement>> = (props) => (
+	<text x="50%" y="50%" dominantBaseline="middle" textAnchor="middle" fontSize={20} {...props} />
 );
 
 // todo: give different sizes to D2-12 so that this has effect
@@ -15,45 +19,56 @@ function adjustImageWidth(width: number, height: number) {
 }
 
 const D2: FC = () => (
-	<DieBaseSvg className={styles.d2}>
+	<DieSvg className={`${styles.d2} ${styles.dice}`}>
 		<circle cx="20" cy="20" r="20" />
-	</DieBaseSvg>
+		<DieText>2</DieText>
+	</DieSvg>
 );
 
 const D4: FC = () => (
-	<DieBaseSvg className={styles.d4}>
+	<DieSvg className={`${styles.d4} ${styles.dice}`}>
 		<path d="M40 37.4H0L20 2.7z" />
-	</DieBaseSvg>
+		<DieText x="47%" y="60%">
+			4
+		</DieText>
+	</DieSvg>
 );
 
 const D6: FC = () => (
-	<DieBaseSvg className={styles.d6}>
+	<DieSvg className={`${styles.d6} ${styles.dice}`}>
 		<path d="M0 0h40v40H0z" />
-	</DieBaseSvg>
+		<DieText x="47%">6</DieText>
+	</DieSvg>
 );
 
 const D8: FC = () => (
-	<DieBaseSvg className={styles.d8}>
+	<DieSvg className={`${styles.d8} ${styles.dice}`}>
 		<path d="M20 40L0 20 20 0l20 20z" />
-	</DieBaseSvg>
+		<DieText x="47%">8</DieText>
+	</DieSvg>
 );
 
 const D10: FC = () => (
-	<DieBaseSvg className={styles.d10}>
+	<DieSvg className={`${styles.d10} ${styles.dice}`}>
 		<path d="M20 0L0 16.9v6.3L20 40l20-16.9V17z" />
-	</DieBaseSvg>
+		<DieText x="47%">10</DieText>
+	</DieSvg>
 );
 
 const D12: FC = () => (
-	<DieBaseSvg className={styles.d12}>
+	<DieSvg className={`${styles.d12} ${styles.dice}`}>
 		<path d="M20 40L0 24.8 7.6 0h24.7L40 24.8z" />
-	</DieBaseSvg>
+		<DieText x="47%" y="45%">
+			12
+		</DieText>
+	</DieSvg>
 );
 
 const D20: FC = () => (
-	<DieBaseSvg className={styles.d20}>
+	<DieSvg className={`${styles.d20} ${styles.dice}`}>
 		<path d="M2.7 30V10L20 0l17.3 10v20L20 40z" />
-	</DieBaseSvg>
+		<DieText y="52%">20</DieText>
+	</DieSvg>
 );
 
 export const diceImages: Record<number, ReactNode> = {
