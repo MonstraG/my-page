6f17e151fbaa1a27f47a055b10690ca5ce25cc23
Arseny Garelyshev <monstrag@gmail.com>
2023-10-12 17:33:57 +0200

    refactor(dice): don't keep every single roll result in memory

--- src/components/DiceRoll/TryRoll.tsx
+++ src/components/DiceRoll/TryRoll.tsx
@@ -6,69 +6,78 @@ function getRandomIntInclusive(min: number, max: number): number {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function rollHistoryToDistribution(
-	min: number,
-	max: number,
-	rolls: number[]
-): Record<number, number> {
+interface RollHistory {
+	latestRolls: readonly number[];
+	distribution: Record<number, number>;
+	count: number;
+}
+
+function probabilityFromRollHistory(rollHistory: RollHistory): Record<number, number> {
+	const probabilityDistribution: Record<number, number> = {};
+	for (const roll in rollHistory.distribution) {
+		probabilityDistribution[roll] = rollHistory.distribution[roll] / rollHistory.count;
+	}
+	return probabilityDistribution;
+}
+
+const getEmptyRollHistory = (dice: readonly number[]): RollHistory => {
+	const minRoll = dice.length;
+	const maxRoll = dice.reduce((acc, next) => acc + next, 0);
+
 	const distribution: Record<number, number> = {};
-	for (let i = min; i <= max; i++) {
+	for (let i = minRoll; i <= maxRoll; i++) {
 		distribution[i] = 0;
 	}
 
-	rolls.forEach((roll) => (distribution[roll] += 1));
-
-	for (let i = min; i <= max; i++) {
-		distribution[i] = distribution[i] / rolls.length;
-	}
-	return distribution;
-}
+	return {
+		latestRolls: [],
+		distribution: {},
+		count: 0
+	};
+};
 
 interface Props {
-	dice: number[];
+	dice: readonly number[];
 }
 
 export const TryRoll: FC<Props> = ({ dice }) => {
-	const [rollHistory, setRollHistory] = useState<number[]>([]);
+	const [rollHistory, setRollHistory] = useState<RollHistory>(getEmptyRollHistory(dice));
 	useEffect(() => {
-		setRollHistory([]);
+		setRollHistory(getEmptyRollHistory(dice));
 	}, [dice]);
 
 	if (dice.length === 0) return null;
+
+	const makeRoll = () => {
+		const newRoll = dice.reduce((acc, dice) => acc + getRandomIntInclusive(1, dice), 0);
+		setRollHistory((prev) => {
+			prev.distribution[newRoll] += 1;
+			return {
+				latestRolls: [...prev.latestRolls.slice(-9), newRoll],
+				count: prev.count + 1,
+				distribution: prev.distribution
+			};
+		});
+	};
 
 	return (
 		<section className={styles.section}>
 			<h2>Try rolling</h2>
 			<div className={styles.stack}>
-				<button
-					className={styles.rollButton}
-					onClick={() => {
-						const newRoll = dice.reduce(
-							(acc, dice) => acc + getRandomIntInclusive(1, dice),
-							0
-						);
-						setRollHistory((prev) => [...prev, newRoll]);
-					}}
-				>
+				<button className={styles.rollButton} onClick={makeRoll}>
 					Try rolling!
 				</button>
-				{rollHistory.length > 0 && (
+				{rollHistory.count > 0 && (
 					<>
 						<div>
 							<h3>Last rolls:</h3>
 							<ul>
-								{rollHistory.slice(-10).map((roll, index) => (
+								{rollHistory.latestRolls.slice(-10).map((roll, index) => (
 									<li key={index}>{roll}</li>
 								))}
 							</ul>
 						</div>
-						<DistributionChart
-							distribution={rollHistoryToDistribution(
-								dice.length,
-								dice.reduce((acc, next) => acc + next, 0),
-								rollHistory
-							)}
-						/>
+						<DistributionChart distribution={probabilityFromRollHistory(rollHistory)} />
 					</>
 				)}
 			</div>
