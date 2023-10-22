5157dcc55636dc7f498a3c0f54ca947be754494c
Arseny Garelyshev <monstrag@gmail.com>
2023-10-22 22:00:12 +0200

    fix: fix dice outline in dark/light mode

--- src/components/DiceRoll/diceImages.tsx
+++ src/components/DiceRoll/diceImages.tsx
@@ -1,5 +1,6 @@
 import type { FC, ReactNode, SVGProps } from "react";
 import styles from "@/components/DiceRoll/diceImages.module.scss";
+import { useColorScheme } from "@mui/joy/styles";
 
 const DieSvg: FC<SVGProps<SVGSVGElement>> = (props) => (
 	<svg width={adjustImageWidth(40, 40)} height="40" viewBox="-1 -1 42 42" {...props} />
@@ -18,58 +19,86 @@ function adjustImageWidth(width: number, height: number) {
 	return Math.pow(imageRatio, scaleFactor) * widthBase;
 }
 
-const D2: FC = () => (
-	<DieSvg className={`${styles.d2} ${styles.dice}`}>
-		<circle cx="20" cy="20" r="20" />
-		<DieText>2</DieText>
-	</DieSvg>
-);
+const D2: FC = () => {
+	const { mode } = useColorScheme();
 
-const D4: FC = () => (
-	<DieSvg className={`${styles.d4} ${styles.dice}`}>
-		<path d="M40 37.4H0L20 2.7z" />
-		<DieText x="47%" y="60%">
-			4
-		</DieText>
-	</DieSvg>
-);
+	return (
+		<DieSvg className={`${styles.d2} ${styles.dice} ${mode === "dark" && styles.diceDark}`}>
+			<circle cx="20" cy="20" r="20" />
+			<DieText>2</DieText>
+		</DieSvg>
+	);
+};
 
-const D6: FC = () => (
-	<DieSvg className={`${styles.d6} ${styles.dice}`}>
-		<path d="M0 0h40v40H0z" />
-		<DieText x="47%">6</DieText>
-	</DieSvg>
-);
+const D4: FC = () => {
+	const { mode } = useColorScheme();
 
-const D8: FC = () => (
-	<DieSvg className={`${styles.d8} ${styles.dice}`}>
-		<path d="M20 40L0 20 20 0l20 20z" />
-		<DieText x="47%">8</DieText>
-	</DieSvg>
-);
+	return (
+		<DieSvg className={`${styles.d4} ${styles.dice} ${mode === "dark" && styles.diceDark}`}>
+			<path d="M40 37.4H0L20 2.7z" />
+			<DieText x="47%" y="60%">
+				4
+			</DieText>
+		</DieSvg>
+	);
+};
 
-const D10: FC = () => (
-	<DieSvg className={`${styles.d10} ${styles.dice}`}>
-		<path d="M20 0L0 16.9v6.3L20 40l20-16.9V17z" />
-		<DieText x="47%">10</DieText>
-	</DieSvg>
-);
+const D6: FC = () => {
+	const { mode } = useColorScheme();
 
-const D12: FC = () => (
-	<DieSvg className={`${styles.d12} ${styles.dice}`}>
-		<path d="M20 40L0 24.8 7.6 0h24.7L40 24.8z" />
-		<DieText x="47%" y="45%">
-			12
-		</DieText>
-	</DieSvg>
-);
+	return (
+		<DieSvg className={`${styles.d6} ${styles.dice} ${mode === "dark" && styles.diceDark}`}>
+			<path d="M0 0h40v40H0z" />
+			<DieText x="47%">6</DieText>
+		</DieSvg>
+	);
+};
 
-const D20: FC = () => (
-	<DieSvg className={`${styles.d20} ${styles.dice}`}>
-		<path d="M2.7 30V10L20 0l17.3 10v20L20 40z" />
-		<DieText y="52%">20</DieText>
-	</DieSvg>
-);
+const D8: FC = () => {
+	const { mode } = useColorScheme();
+
+	return (
+		<DieSvg className={`${styles.d8} ${styles.dice} ${mode === "dark" && styles.diceDark}`}>
+			<path d="M20 40L0 20 20 0l20 20z" />
+			<DieText x="47%">8</DieText>
+		</DieSvg>
+	);
+};
+
+const D10: FC = () => {
+	const { mode } = useColorScheme();
+
+	return (
+		<DieSvg className={`${styles.d10} ${styles.dice} ${mode === "dark" && styles.diceDark}`}>
+			<path d="M20 0L0 16.9v6.3L20 40l20-16.9V17z" />
+			<DieText x="47%">10</DieText>
+		</DieSvg>
+	);
+};
+
+const D12: FC = () => {
+	const { mode } = useColorScheme();
+
+	return (
+		<DieSvg className={`${styles.d12} ${styles.dice} ${mode === "dark" && styles.diceDark}`}>
+			<path d="M20 40L0 24.8 7.6 0h24.7L40 24.8z" />
+			<DieText x="47%" y="45%">
+				12
+			</DieText>
+		</DieSvg>
+	);
+};
+
+const D20: FC = () => {
+	const { mode } = useColorScheme();
+
+	return (
+		<DieSvg className={`${styles.d20} ${styles.dice} ${mode === "dark" && styles.diceDark}`}>
+			<path d="M2.7 30V10L20 0l17.3 10v20L20 40z" />
+			<DieText y="52%">20</DieText>
+		</DieSvg>
+	);
+};
 
 export const diceImages: Record<number, ReactNode> = {
 	2: <D2 />,
