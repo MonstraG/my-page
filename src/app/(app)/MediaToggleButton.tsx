0484d35382abf2069410e15d7d1ab66160fcfeb4
Arseny Garelyshev <monstrag@gmail.com>
2023-10-22 21:44:58 +0200

    feat: icons for MediaToggleButton.tsx

--- src/app/(app)/MediaToggleButton.tsx
+++ src/app/(app)/MediaToggleButton.tsx
@@ -3,18 +3,29 @@
 import { type FC } from "react";
 import { useColorScheme } from "@mui/joy/styles";
 import Button from "@mui/joy/Button";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
 
 export const MediaToggleButton: FC = () => {
 	const { mode, setMode } = useColorScheme();
 	return (
-		<Button
-			variant="outlined"
-			color="neutral"
-			onClick={() => {
-				setMode(mode === "dark" ? "light" : "dark");
-			}}
-		>
-			{mode === "dark" ? "Turn light" : "Turn dark"}
-		</Button>
+		<div>
+			<Button
+				color="neutral"
+				onClick={() => {
+					setMode(mode === "dark" ? "light" : "dark");
+				}}
+				size="sm"
+				variant="plain"
+				sx={{
+					paddingInline: 0.75,
+					position: "absolute",
+					right: 8,
+					top: 8
+				}}
+			>
+				{mode === "dark" ? <LightModeIcon /> : <DarkModeIcon />}
+			</Button>
+		</div>
 	);
 };
