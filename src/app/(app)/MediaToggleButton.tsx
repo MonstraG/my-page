941118b4cc3f76363bc30d538e47252f9f61f877
Arseny Garelyshev <monstrag@gmail.com>
2023-10-27 21:00:42 +0200

    fix: render html mismatch

--- src/app/(app)/MediaToggleButton.tsx
+++ src/app/(app)/MediaToggleButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { type FC } from "react";
+import { type FC, useEffect, useState } from "react";
 import { useColorScheme } from "@mui/joy/styles";
 import Button from "@mui/joy/Button";
 import LightModeIcon from "@mui/icons-material/LightMode";
@@ -8,6 +8,31 @@ import DarkModeIcon from "@mui/icons-material/DarkMode";
 
 export const MediaToggleButton: FC = () => {
 	const { mode, setMode } = useColorScheme();
+	const [rendered, setRendred] = useState<boolean>(false);
+	useEffect(() => {
+		if (typeof window !== "undefined") {
+			setRendred(true);
+		}
+	}, []);
+
+	if (!rendered) {
+		return (
+			<Button
+				color="neutral"
+				disabled
+				size="sm"
+				variant="plain"
+				sx={{
+					paddingInline: 0.75,
+					position: "absolute",
+					right: 8,
+					top: 8
+				}}
+			>
+				<LightModeIcon />
+			</Button>
+		);
+	}
 
 	return (
 		<Button
