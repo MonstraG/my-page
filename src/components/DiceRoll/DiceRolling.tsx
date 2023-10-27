6c4a83adb2e0242faa6fa54bdf4d071bd9dbfe6d
Arseny Garelyshev <monstrag@gmail.com>
2023-10-27 19:26:39 +0200

    refactor(dice): extract DiceSelection section

--- src/components/DiceRoll/DiceRolling.tsx
+++ src/components/DiceRoll/DiceRolling.tsx
@@ -1,11 +1,8 @@
 "use client";
 import { type FC, useState } from "react";
-import styles from "@/components/DiceRoll/DiceRoll.module.scss";
 import { Distribution } from "@/components/DiceRoll/Distribution";
-import { DiceBag } from "@/components/DiceRoll/DiceBag";
 import { TryRoll } from "@/components/DiceRoll/TryRoll/TryRoll";
-
-const possibleDice = [2, 4, 6, 8, 10, 12, 20];
+import { DiceSelection } from "@/components/DiceRoll/DiceSelection";
 
 export const DiceRolling: FC = () => {
 	const [selectedDice, setSelectedDice] = useState<number[]>([]);
@@ -13,26 +10,8 @@ export const DiceRolling: FC = () => {
 	return (
 		<>
 			<h1>Dice rolling</h1>
-			<section>
-				<h2>Select dice</h2>
-				<div className={styles.diceControls}>
-					<DiceBag
-						title="Add dice"
-						dice={possibleDice}
-						onDiceClick={(die) => {
-							setSelectedDice((p) => [...p, die].toSorted((a, b) => a - b));
-						}}
-					/>
 
-					<DiceBag
-						title="Remove dice"
-						dice={selectedDice}
-						onDiceClick={(_, index) => {
-							setSelectedDice((p) => p.toSpliced(index, 1));
-						}}
-					/>
-				</div>
-			</section>
+			<DiceSelection selectedDice={selectedDice} setSelectedDice={setSelectedDice} />
 
 			<Distribution dice={selectedDice} />
 
