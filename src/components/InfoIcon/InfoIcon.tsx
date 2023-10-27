f0d8e0d869ea0485417912481098c30a6911b884
Arseny Garelyshev <monstrag@gmail.com>
2023-10-27 23:14:45 +0200

    chore: remove unused InfoIcon

--- src/components/InfoIcon/InfoIcon.tsx
+++
@@ -1,8 +0,0 @@
-import type { FC, HTMLAttributes } from "react";
-import styles from "@/components/InfoIcon/InfoIcon.module.scss";
-
-export const InfoIcon: FC<HTMLAttributes<HTMLDivElement>> = (props) => (
-	<div className={styles.icon} {...props}>
-		i
-	</div>
-);
