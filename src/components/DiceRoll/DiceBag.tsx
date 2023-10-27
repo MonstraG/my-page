2409334790673b86f812c3ebd4600ab55a7bee19
Arseny Garelyshev <monstrag@gmail.com>
2023-10-27 19:02:53 +0200

    fix(dice): screw this, dice are always with dark outline now

--- src/components/DiceRoll/DiceBag.tsx
+++ src/components/DiceRoll/DiceBag.tsx
@@ -1,6 +1,6 @@
-import type { FC, ReactNode } from "react";
+import type { FC } from "react";
 import styles from "@/components/DiceRoll/DiceRoll.module.scss";
-import { D10, D12, D2, D20, D4, D6, D8 } from "@/components/DiceRoll/diceImages";
+import { diceImages } from "@/components/DiceRoll/diceImages";
 
 function groupSame(array: number[]): number[][] {
 	return array.reduce<number[][]>((acc, val) => {
@@ -19,38 +19,25 @@ interface Props {
 	onDiceClick: (die: number, index: number) => void;
 }
 
-export const DiceBag: FC<Props> = ({ title, dice, onDiceClick }) => {
-	// must be declared in render function to make sure it updates from the theme mode
-	const diceImages: Record<number, ReactNode> = {
-		2: <D2 />,
-		4: <D4 />,
-		6: <D6 />,
-		8: <D8 />,
-		10: <D10 />,
-		12: <D12 />,
-		20: <D20 />
-	};
-
-	return (
-		<div className={styles.diceSet}>
-			<h3>{title}</h3>
-			<div className={styles.diceButtons}>
-				{groupSame(dice).map((sameSideDice, groupIndex) => (
-					<div key={groupIndex}>
-						{sameSideDice.map((die, dieIndex) => (
-							<button
-								key={dieIndex}
-								onClick={() => {
-									onDiceClick(die, dieIndex);
-								}}
-								className={styles.dieButton}
-							>
-								{diceImages[die]}
-							</button>
-						))}
-					</div>
-				))}
-			</div>
+export const DiceBag: FC<Props> = ({ title, dice, onDiceClick }) => (
+	<div className={styles.diceSet}>
+		<h3>{title}</h3>
+		<div className={styles.diceButtons}>
+			{groupSame(dice).map((sameSideDice, groupIndex) => (
+				<div key={groupIndex}>
+					{sameSideDice.map((die, dieIndex) => (
+						<button
+							key={dieIndex}
+							onClick={() => {
+								onDiceClick(die, dieIndex);
+							}}
+							className={styles.dieButton}
+						>
+							{diceImages[die]}
+						</button>
+					))}
+				</div>
+			))}
 		</div>
-	);
-};
+	</div>
+);
