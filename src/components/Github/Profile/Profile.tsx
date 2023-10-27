803c663a5471a2bf17d34d17f3cf486a5cd750b3
Arseny Garelyshev <monstrag@gmail.com>
2023-10-27 23:13:33 +0200

    refactor(profile): rewrite Profile.tsx with mui/joy

--- src/components/Github/Profile/Profile.tsx
+++ src/components/Github/Profile/Profile.tsx
@@ -1,32 +1,36 @@
 import type { FC } from "react";
 import { getProfile } from "@/components/Github/Profile/getProfile";
 import Image from "next/image";
-import styles from "@/components/Github/Profile/Profile.module.scss";
-import { Link, Stack, Typography } from "@mui/joy";
+import { Link, Stack, Avatar, Typography } from "@mui/joy";
 
 export const Profile: FC = async () => {
 	const profile = await getProfile();
 
+	const subtitle = [
+		profile.company ? `Working at ${profile.company}` : "",
+		profile.location ? `living in ${profile.location}` : ""
+	].join(", ");
+
 	return (
 		<Stack direction="row" alignItems="center" spacing={2} my={4}>
-			{profile.avatarUrl && (
-				<Image
-					src={profile.avatarUrl}
-					alt={profile.name + "'s avatar"}
-					height={48}
-					width={48}
-					className={styles.avatar}
-				/>
-			)}
-			<div className={styles.nameAndSubtitle}>
+			<Avatar size="lg" variant="outlined">
+				{profile.avatarUrl && (
+					<Image
+						src={profile.avatarUrl}
+						alt={profile.name + "'s avatar"}
+						height={48}
+						width={48}
+					/>
+				)}
+			</Avatar>
+			<Stack direction="row" alignItems="baseline" spacing={2}>
 				<Typography level="h1">
 					<Link href={profile.url} rel="noopener noreferrer nofollow ugc">
 						{profile.name}
 					</Link>
 				</Typography>
-				{profile.company && <Typography>Working at {profile.company}</Typography>}
-				{profile.location && <Typography>{profile.location}</Typography>}
-			</div>
+				<Typography>{subtitle}</Typography>
+			</Stack>
 		</Stack>
 	);
 };
