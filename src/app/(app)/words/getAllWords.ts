d5c4b465021e697fd5a376ed46293a78e2020cd5
Arseny Garelyshev <monstrag@gmail.com>
2023-12-05 19:08:39 +0100

    feat: only send first 20k words, 50k is a bit too much

--- src/app/(app)/words/getAllWords.ts
+++ src/app/(app)/words/getAllWords.ts
@@ -1,5 +1,24 @@
-import { promises as fsPromises } from "fs";
+import fs from "fs";
+import readline from "readline";
 
-export async function getAllWords() {
-	return fsPromises.readFile(`public/50k.txt`, "utf8");
+export async function getAllWords(): Promise<string[]> {
+	const fileStream = fs.createReadStream("public/50k.txt", "utf8");
+
+	const rl = readline.createInterface({
+		input: fileStream,
+		crlfDelay: Infinity
+	});
+
+	const lines = [];
+	for await (const line of rl) {
+		// When 20000 lines are read, stop reading further by breaking the loop
+		if (lines.length === 20000) {
+			break;
+		}
+
+		// and this is where I cry about lack of Array.from(AsyncIterator).
+		lines.push(line);
+	}
+
+	return lines;
 }
