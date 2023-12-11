2e12058b6a263cc1e11bfaa5093277eff8deb736
Arseny Garelyshev <monstrag@gmail.com>
2023-12-11 17:08:31 +0100

    feat: change index again, send to /me in the cv.

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,6 +1,21 @@
+import Container from "@mui/joy/Container";
 import type { NextPage } from "next";
-import { DiceRolling } from "@/components/DiceRoll/DiceRolling";
+import Typography from "@mui/joy/Typography";
 
-const IndexPage: NextPage = () => <DiceRolling />;
+const IndexPage: NextPage = () => {
+	return (
+		<Container sx={{ py: 10 }}>
+			<Typography level="h1" gutterBottom>
+				If you landed here and expected to find something, make sure that your link is
+				correct.
+			</Typography>
+			<Typography level="h2" gutterBottom>
+				Eventually, I&apos;ll put some things up here for you too click around in, but not
+				just yet.
+			</Typography>
+			<Typography level="h2">Have a nice day!</Typography>
+		</Container>
+	);
+};
 
 export default IndexPage;
