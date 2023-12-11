c677950c92402126a76cace677e60c6c9b5c27ac
Arseny Garelyshev <monstrag@gmail.com>
2023-12-11 21:21:31 +0100

    feat: linearize dictionary api entry viewing experience

---
+++ src/app/(app)/words/DictionaryApi/useDictionaryApi.tsx
@@ -0,0 +1,70 @@
+import { useCallback, useState } from "react";
+import type {
+	Dictionary,
+	DictionaryApiResponse
+} from "@/app/(app)/words/DictionaryApi/DictionaryApi.types";
+import { emptyDictionary } from "@/app/(app)/words/DictionaryApi/DictionaryApiViewer";
+import { openSnackbar } from "@/components/SnackbarHost";
+import {
+	isResponseErroneous,
+	parseMeanings
+} from "@/app/(app)/words/DictionaryApi/DictionaryApi.helpers";
+
+const definitionUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+export const useDictionaryApi = () => {
+	const [dictionary, setDictionary] = useState<Dictionary>(emptyDictionary);
+	const [loadingDefinitions, setLoadingDefinitions] = useState<boolean>(false);
+
+	const fetchDefinition = useCallback((word: string) => {
+		setLoadingDefinitions(true);
+		fetch(`${definitionUrl}${word}`, { method: "GET" })
+			.then((response) => response.json())
+			.then((data: DictionaryApiResponse) => {
+				if (isResponseErroneous(data)) {
+					setDictionary({ ...emptyDictionary, meanings: "not found" });
+					return;
+				}
+
+				setDictionary({ ...emptyDictionary, meanings: parseMeanings(data) });
+			})
+			.catch((err) => {
+				console.error(err);
+				openSnackbar({
+					color: "danger",
+					variant: "solid",
+					children: "Failed to get the definition"
+				});
+			})
+			.finally(() => {
+				setLoadingDefinitions(false);
+			});
+	}, []);
+
+	const clearDictionary = useCallback(() => {
+		setDictionary(emptyDictionary);
+	}, []);
+
+	const toPreviousMeaning = useCallback(() => {
+		setDictionary((prev) => ({
+			...prev,
+			index: prev.index - 1
+		}));
+	}, []);
+
+	const toNextMeaning = useCallback(() => {
+		setDictionary((prev) => ({
+			...prev,
+			index: prev.index + 1
+		}));
+	}, []);
+
+	return {
+		dictionary,
+		loadingDefinitions,
+		fetchDefinition,
+		clearDictionary,
+		toPreviousMeaning,
+		toNextMeaning
+	};
+};
