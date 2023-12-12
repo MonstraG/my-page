01be0c66e20d4964f8d77b4474da64f5bfe9141b
Arseny Garelyshev <monstrag@gmail.com>
2023-12-12 21:22:40 +0100

    f

--- src/app/(app)/useless.tsx
+++ src/app/(app)/useless.tsx
@@ -1,1 +1,22 @@
 test
+code;
+
+code code;
+
+code coed ecode code;
+
+words;
+
+I love writing code;
+
+i lowe reviewing pull requests even more;
+
+but I hate red code, becuase it is erronious.
+
+code;
+
+code code;
+
+code coed ecode code;
+
+words;
