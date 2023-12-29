292c204eec4c9f4427b888902e4002b2c2374146
Arseny Garelyshev <monstrag@gmail.com>
2023-12-29 20:58:14 +0100

    feat: show if a spell requires concentration

--- src/app/(app)/dnd-spells/SpellListItem.tsx
+++ src/app/(app)/dnd-spells/SpellListItem.tsx
@@ -1,6 +1,8 @@
 import type { FC } from "react";
 import ListItemButton from "@mui/joy/ListItemButton";
 import type { Spell } from "@/app/(app)/dnd-spells/spells/spells.types";
+import Typography from "@mui/joy/Typography";
+import { ConcentrationChip } from "@/app/(app)/dnd-spells/ConcentrationChip";
 
 interface Props {
 	spell: Spell;
@@ -13,6 +15,12 @@ export const SpellListItem: FC<Props> = ({ spell, onClick }) => (
 			onClick(spell);
 		}}
 	>
-		({spell.level}) {spell.title} ({spell.titleEn})
+		<Typography>
+			[
+			<Typography component="span" level="body-sm" fontWeight="lg">
+				{spell.level}
+			</Typography>
+			] {spell.title} ({spell.titleEn}) {spell.concentration && <ConcentrationChip short />}
+		</Typography>
 	</ListItemButton>
 );
