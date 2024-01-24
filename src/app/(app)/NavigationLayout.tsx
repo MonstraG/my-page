48cfd2846e4539c4be5bf34f5f1775ac1ec7a3cb
Arseny Garelyshev <monstrag@gmail.com>
2024-01-24 20:19:53 +0100

    feat: smaller horizontal size of drawer

--- src/app/(app)/NavigationLayout.tsx
+++ src/app/(app)/NavigationLayout.tsx
@@ -4,7 +4,6 @@ import { SnackbarHost } from "@/components/SnackbarHost";
 import { NavDrawer } from "@/app/(app)/NavDrawer";
 import type { FCC } from "@/types/react";
 import { useState } from "react";
-import Sheet from "@mui/joy/Sheet";
 import Stack from "@mui/joy/Stack";
 import MenuIcon from "@mui/icons-material/Menu";
 import IconButton from "@mui/joy/IconButton";
@@ -13,24 +12,31 @@ export const NavigationLayout: FCC = ({ children }) => {
 	const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
 	return (
 		<>
-			<Sheet
+			<Stack
+				direction="row"
+				justifyContent="space-between"
+				alignItems="center"
 				component="header"
-				sx={{ position: "sticky", top: 0, p: 1, zIndex: 10, elevation: 1 }}
+				sx={{
+					position: "sticky",
+					top: 0,
+					p: 1,
+					zIndex: 10,
+					elevation: 1
+				}}
 			>
-				<Stack direction="row" justifyContent="space-between" alignItems="center">
-					<IconButton
-						variant="outlined"
-						color="neutral"
-						onClick={() => {
-							setDrawerOpen(true);
-						}}
-					>
-						<MenuIcon />
-					</IconButton>
+				<IconButton
+					variant="outlined"
+					color="neutral"
+					onClick={() => {
+						setDrawerOpen(true);
+					}}
+				>
+					<MenuIcon />
+				</IconButton>
 
-					<MediaToggleButton />
-				</Stack>
-			</Sheet>
+				<MediaToggleButton />
+			</Stack>
 			<main>{children}</main>
 			<SnackbarHost />
 			<NavDrawer
