b19349290776b6761b8eb21495806dca36e5a521
Arseny Garelyshev <monstrag@gmail.com>
2024-01-24 20:19:53 +0100

    feat: smaller horizontal size of drawer

--- src/app/(app)/NavDrawer.tsx
+++ src/app/(app)/NavDrawer.tsx
@@ -13,30 +13,38 @@ interface Props {
 }
 
 export const NavDrawer: FC<Props> = ({ isOpen, onClose }) => (
-	<>
-		<Drawer open={isOpen} onClose={onClose} sx={{ position: "relative" }} size="sm">
-			<Stack direction="row" p={1} pb={0} gap={1} display="flex" justifyContent="flex-end">
-				<Typography>Close</Typography>
-				<ModalClose sx={{ position: "initial" }} />
-			</Stack>
+	<Drawer
+		open={isOpen}
+		onClose={onClose}
+		sx={{
+			position: "relative",
+			"--Drawer-transitionDuration": "0.2s",
+			"--Drawer-horizontalSize": "256px"
+		}}
+		size="sm"
+		hideBackdrop
+	>
+		<Stack direction="row" p={1} pb={0} gap={1} display="flex" justifyContent="flex-end">
+			<Typography>Close</Typography>
+			<ModalClose sx={{ position: "initial" }} />
+		</Stack>
 
-			<List size="lg" component="nav" sx={{ width: "100%", p: 0 }}>
-				<NextLink href="/" legacyBehavior>
-					<ListItemButton component="a" onClick={onClose}>
-						Dice rolling
-					</ListItemButton>
-				</NextLink>
-				<NextLink href="/words" legacyBehavior>
-					<ListItemButton component="a" onClick={onClose}>
-						Vocabulary tester
-					</ListItemButton>
-				</NextLink>
-				<NextLink href="/dnd-spells" legacyBehavior>
-					<ListItemButton component="a" onClick={onClose}>
-						DnD spells
-					</ListItemButton>
-				</NextLink>
-			</List>
-		</Drawer>
-	</>
+		<List size="lg" component="nav" sx={{ width: "100%", p: 0 }}>
+			<NextLink href="/" legacyBehavior>
+				<ListItemButton component="a" onClick={onClose}>
+					Dice rolling
+				</ListItemButton>
+			</NextLink>
+			<NextLink href="/words" legacyBehavior>
+				<ListItemButton component="a" onClick={onClose}>
+					Vocabulary tester
+				</ListItemButton>
+			</NextLink>
+			<NextLink href="/dnd-spells" legacyBehavior>
+				<ListItemButton component="a" onClick={onClose}>
+					DnD spells
+				</ListItemButton>
+			</NextLink>
+		</List>
+	</Drawer>
 );
