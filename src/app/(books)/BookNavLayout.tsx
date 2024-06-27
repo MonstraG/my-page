83470739cf22434469541516393cfed7b1d551e0
Arseny Garelyshev <monstrag@gmail.com>
2024-06-27 21:35:50 +0200

    feat: 4 font family choices

--- src/app/(books)/BookNavLayout.tsx
+++ src/app/(books)/BookNavLayout.tsx
@@ -11,59 +11,14 @@ import Box from "@mui/joy/Box";
 import type { SxProps } from "@mui/joy/styles/types";
 import { useBookControlsStore } from "@/app/(books)/useBookControlsStore";
 import { useColorScheme } from "@mui/joy/styles";
-import SettingsIcon from "@mui/icons-material/Settings";
-import Button from "@mui/joy/Button";
-import MenuButton from "@mui/joy/MenuButton";
-import Menu from "@mui/joy/Menu";
-import Dropdown from "@mui/joy/Dropdown";
-import MenuItem from "@mui/joy/MenuItem";
 import { useHasRendered } from "@/components/useHasRendered";
-import CheckIcon from "@mui/icons-material/Check";
-import ListItemDecorator from "@mui/joy/ListItemDecorator";
-import AddIcon from "@mui/icons-material/Add";
-import RemoveIcon from "@mui/icons-material/Remove";
-import ListItem from "@mui/joy/ListItem";
 import Container from "@mui/joy/Container";
 import { EB_Garamond } from "next/font/google";
-
-const minFontSize = 10;
-const maxFontSize = 30;
-const minFontWeight = 300;
-const maxFontWeight = 600;
-
-const handleWideModeToggle = () => {
-	useBookControlsStore.setState((prev) => ({ wide: !prev.wide }));
-};
-const handleJustifyToggle = () => {
-	useBookControlsStore.setState((prev) => ({ justify: !prev.justify }));
-};
-const handleFontSizeDecrease = () => {
-	useBookControlsStore.setState((prev) => ({
-		fontSize: Math.max(prev.fontSize - 1, minFontSize)
-	}));
-};
-const handleFontSizeIncrease = () => {
-	useBookControlsStore.setState((prev) => ({
-		fontSize: Math.min(prev.fontSize + 1, maxFontSize)
-	}));
-};
-const handleFontWeightDecrease = () => {
-	useBookControlsStore.setState((prev) => ({
-		fontWeight: Math.max(prev.fontWeight - 100, minFontWeight)
-	}));
-};
-const handleFontWeightIncrease = () => {
-	useBookControlsStore.setState((prev) => ({
-		fontWeight: Math.min(prev.fontWeight + 100, maxFontWeight)
-	}));
-};
-const handleSansSerifToggle = () => {
-	useBookControlsStore.setState((prev) => ({ sansSerif: !prev.sansSerif }));
-};
+import { BookSettings } from "@/app/(books)/BookSettings";
 
 const garamond = EB_Garamond({
 	subsets: ["latin", "cyrillic", "cyrillic-ext"],
-	adjustFontFallback: false, // prevent NextJS from adding its own fallback font
+	adjustFontFallback: false, // prevent Next.js from adding its own fallback font
 	fallback: ["var(--joy-fontFamily-fallback)"], // use Joy UI's fallback font
 	display: "swap"
 });
@@ -74,19 +29,34 @@ const sepiaSx: SxProps = {
 };
 
 export const BookNavLayout: FCC = ({ children }) => {
-	const { wide, justify, fontSize, fontWeight, sansSerif } = useBookControlsStore();
-
+	const { wide, justify, fontSize, fontWeight, fontFamily } = useBookControlsStore();
 	const { mode } = useColorScheme();
 
 	const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
-	const [settingsOpen, setSettingsOpen] = useState<boolean>(false);
 
 	const rendered = useHasRendered();
 	if (!rendered) {
 		return null;
 	}
 
-	const fontFamilyStyles = sansSerif ? {} : garamond.style;
+	const fontFamilyStyles = (() => {
+		switch (fontFamily) {
+			case "Garamond":
+				return garamond.style;
+			case "Inter":
+				return {
+					// default font for website
+				};
+			case "system sans-serif":
+				return {
+					fontFamily: "sans-serif"
+				};
+			case "system serif":
+				return {
+					fontFamily: "serif"
+				};
+		}
+	})();
 
 	return (
 		<Box sx={mode === "light" ? sepiaSx : undefined}>
@@ -115,119 +85,7 @@ export const BookNavLayout: FCC = ({ children }) => {
 
 				<div>
 					<ColorSchemeToggleButton />
-
-					<Dropdown
-						open={settingsOpen}
-						onOpenChange={(event, newOpen) => {
-							if (event != null && !newOpen && event.type === "click") {
-								return;
-							}
-							setSettingsOpen(newOpen);
-						}}
-					>
-						<MenuButton
-							slots={{ root: Button }}
-							slotProps={{
-								root: {
-									variant: "plain",
-									color: "neutral",
-									size: "sm",
-									sx: { px: 0.75 }
-								}
-							}}
-						>
-							<SettingsIcon />
-						</MenuButton>
-						<Menu placement="bottom-end">
-							<MenuItem
-								onClick={handleWideModeToggle}
-								sx={{ justifyContent: "space-between" }}
-							>
-								Use wide mode
-								<ListItemDecorator sx={{ minWidth: "unset", pr: 1 }}>
-									<CheckIcon sx={{ visibility: wide ? undefined : "hidden" }} />
-								</ListItemDecorator>
-							</MenuItem>
-							<MenuItem
-								onClick={handleJustifyToggle}
-								sx={{ justifyContent: "space-between" }}
-							>
-								Justify text
-								<ListItemDecorator sx={{ minWidth: "unset", pr: 1 }}>
-									<CheckIcon
-										sx={{ visibility: justify ? undefined : "hidden" }}
-									/>
-								</ListItemDecorator>
-							</MenuItem>
-							<ListItem sx={{ justifyContent: "space-between" }}>
-								Font size
-								<ListItemDecorator sx={{ minWidth: "unset", pr: 1, gap: 0.5 }}>
-									<Button
-										variant="plain"
-										color="neutral"
-										size="sm"
-										sx={{ px: 0.5 }}
-										onClick={handleFontSizeDecrease}
-										disabled={fontSize <= minFontSize}
-									>
-										<RemoveIcon />
-									</Button>
-
-									{fontSize}
-
-									<Button
-										variant="plain"
-										color="neutral"
-										size="sm"
-										sx={{ px: 0.5 }}
-										onClick={handleFontSizeIncrease}
-										disabled={fontSize >= maxFontSize}
-									>
-										<AddIcon />
-									</Button>
-								</ListItemDecorator>
-							</ListItem>
-							<ListItem sx={{ justifyContent: "space-between" }}>
-								Font weight
-								<ListItemDecorator sx={{ minWidth: "unset", pr: 1, gap: 0.5 }}>
-									<Button
-										variant="plain"
-										color="neutral"
-										size="sm"
-										sx={{ px: 0.5 }}
-										onClick={handleFontWeightDecrease}
-										disabled={fontSize <= minFontSize}
-									>
-										<RemoveIcon />
-									</Button>
-
-									{fontWeight / 100}
-
-									<Button
-										variant="plain"
-										color="neutral"
-										size="sm"
-										sx={{ px: 0.5 }}
-										onClick={handleFontWeightIncrease}
-										disabled={fontSize >= maxFontSize}
-									>
-										<AddIcon />
-									</Button>
-								</ListItemDecorator>
-							</ListItem>
-							<MenuItem
-								onClick={handleSansSerifToggle}
-								sx={{ justifyContent: "space-between" }}
-							>
-								Use sans-serif font
-								<ListItemDecorator sx={{ minWidth: "unset", pr: 1 }}>
-									<CheckIcon
-										sx={{ visibility: sansSerif ? undefined : "hidden" }}
-									/>
-								</ListItemDecorator>
-							</MenuItem>
-						</Menu>
-					</Dropdown>
+					<BookSettings />
 				</div>
 			</Stack>
 			<Container
