1cfedb4ab2ffea05eb1139c44344684a4d3df315
Arseny Garelyshev <monstrag@gmail.com>
2024-07-26 17:57:49 +0200

    refactor: download images for articles, etc.

---
+++ src/app/(app)/ideas-somebody-already-made/ArticleWithMainImage.tsx
@@ -0,0 +1,21 @@
+import Typography from "@mui/joy/Typography";
+import Image, { type ImageProps } from "next/image";
+import type { FCC } from "@/types/react";
+import Stack from "@mui/joy/Stack";
+
+interface Props {
+	title: string;
+	ImageProps: ImageProps;
+}
+
+export const ArticleWithMainImage: FCC<Props> = ({ title, ImageProps, children }) => (
+	<section>
+		<Typography level="h2" gutterBottom>
+			{title}
+		</Typography>
+		<Stack sx={{ my: 8 }} alignItems="center">
+			<Image {...ImageProps} />
+		</Stack>
+		<Stack gap={2}>{children}</Stack>
+	</section>
+);
