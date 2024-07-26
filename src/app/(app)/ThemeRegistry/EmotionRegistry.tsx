51f614e44b342549dfa391da65ecbf38d97a6b33
Arseny Garelyshev <monstrag@gmail.com>
2024-07-26 17:34:44 +0200

    chore: update deps and Emotion setup

--- src/app/(app)/ThemeRegistry/EmotionRegistry.tsx
+++ src/app/(app)/ThemeRegistry/EmotionRegistry.tsx
@@ -2,7 +2,7 @@
 import * as React from "react";
 import createCache from "@emotion/cache";
 import { useServerInsertedHTML } from "next/navigation";
-import { CacheProvider as DefaultCacheProvider } from "@emotion/react";
+import { CacheProvider } from "@emotion/react";
 import type { EmotionCache, Options as OptionsOfCreateCache } from "@emotion/cache";
 
 export interface NextAppDirEmotionCacheProviderProps {
@@ -13,24 +13,18 @@ export interface NextAppDirEmotionCacheProviderProps {
 	children: React.ReactNode;
 }
 
-// Adapted from https://github.com/garronej/tss-react/blob/main/src/next/appDir.tsx
 export default function NextAppDirEmotionCacheProvider(props: NextAppDirEmotionCacheProviderProps) {
-	const { options, CacheProvider = DefaultCacheProvider, children } = props;
+	const { options, children } = props;
 
-	const [registry] = React.useState(() => {
+	const [{ cache, flush }] = React.useState(() => {
 		const cache = createCache(options);
 		cache.compat = true;
-		// eslint-disable-next-line @typescript-eslint/unbound-method
 		const prevInsert = cache.insert;
-		let inserted: { name: string; isGlobal: boolean }[] = [];
+		let inserted: string[] = [];
 		cache.insert = (...args) => {
-			const [selector, serialized] = args;
-			// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+			const serialized = args[1];
 			if (cache.inserted[serialized.name] === undefined) {
-				inserted.push({
-					name: serialized.name,
-					isGlobal: !selector
-				});
+				inserted.push(serialized.name);
 			}
 			return prevInsert(...args);
 		};
@@ -43,51 +37,24 @@ export default function NextAppDirEmotionCacheProvider(props: NextAppDirEmotionC
 	});
 
 	useServerInsertedHTML(() => {
-		const inserted = registry.flush();
-		if (inserted.length === 0) {
+		const names = flush();
+		if (names.length === 0) {
 			return null;
 		}
 		let styles = "";
-		let dataEmotionAttribute = registry.cache.key;
-
-		const globals: {
-			name: string;
-			style: string;
-		}[] = [];
-
-		inserted.forEach(({ name, isGlobal }) => {
-			const style = registry.cache.inserted[name];
-
-			if (typeof style !== "boolean") {
-				if (isGlobal) {
-					globals.push({ name, style });
-				} else {
-					styles += style;
-					dataEmotionAttribute += ` ${name}`;
-				}
-			}
-		});
-
+		for (const name of names) {
+			styles += (cache.inserted[name] ?? "").toString();
+		}
 		return (
-			<React.Fragment>
-				{globals.map(({ name, style }) => (
-					<style
-						key={name}
-						data-emotion={`${registry.cache.key}-global ${name}`}
-						// eslint-disable-next-line react/no-danger
-						dangerouslySetInnerHTML={{ __html: style }}
-					/>
-				))}
-				{styles && (
-					<style
-						data-emotion={dataEmotionAttribute}
-						// eslint-disable-next-line react/no-danger
-						dangerouslySetInnerHTML={{ __html: styles }}
-					/>
-				)}
-			</React.Fragment>
+			<style
+				key={cache.key}
+				data-emotion={`${cache.key} ${names.join(" ")}`}
+				dangerouslySetInnerHTML={{
+					__html: styles
+				}}
+			/>
 		);
 	});
 
-	return <CacheProvider value={registry.cache}>{children}</CacheProvider>;
+	return <CacheProvider value={cache}>{children}</CacheProvider>;
 }
