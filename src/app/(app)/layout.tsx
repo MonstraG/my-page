9ad104eccb9d952801545a295c5f50ff6e652734
Arseny Garelyshev <monstrag@gmail.com>
2025-01-14 22:12:55 +0100

    feat: delete books, noone needs it

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,15 +1,11 @@
 import type { FCC } from "@/types/react";
 import { NavigationLayout } from "@/components/nav/NavigationLayout";
 import { ThemeRegistry } from "@/components/theme/ThemeRegistry";
-import { setHtmlLangHack } from "@/app/evilHtmlLangHack";
 
-const AppLayout: FCC = ({ children }) => {
-	setHtmlLangHack("en");
-	return (
-		<ThemeRegistry>
-			<NavigationLayout>{children}</NavigationLayout>
-		</ThemeRegistry>
-	);
-};
+const AppLayout: FCC = ({ children }) => (
+	<ThemeRegistry>
+		<NavigationLayout>{children}</NavigationLayout>
+	</ThemeRegistry>
+);
 
 export default AppLayout;
