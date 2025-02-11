9a2af9b81ec8d8fe60f85b5a26c9ca14c9ce0760
Arseny Garelyshev <monstrag@gmail.com>
2025-02-11 21:18:06 +0100

    refactor: replace swr with just server side render

---
+++ src/app/(app)/words/[language]/page.tsx
@@ -0,0 +1,35 @@
+import type { Metadata, NextPage } from "next";
+import { VocabularyTester } from "@/components/words/VocabularyTester";
+import Container from "@mui/joy/Container";
+import { promises as fs } from "fs";
+import { type Language, languages } from "@/components/words/useWordsStore";
+
+export async function generateStaticParams(): Promise<{ language: Language }[]> {
+	const dirEntries = await fs.readdir("words");
+	return dirEntries
+		.filter((file) => file.endsWith(".txt"))
+		.map((wordFile) => wordFile.replace(".txt", ""))
+		.filter((fileName): fileName is Language => languages.some((l) => l.iso === fileName))
+		.map((language) => ({ language }));
+}
+
+export const metadata: Metadata = {
+	title: "Vocabulary Tester"
+};
+
+const VocabularyTesterPage: NextPage<{ params: Promise<{ language: Language }> }> = async ({
+	params
+}) => {
+	const { language } = await params;
+
+	const file = await fs.readFile(`words/${language}.txt`, "utf-8");
+	const allWords = file.split("\n");
+
+	return (
+		<Container sx={{ py: 4 }}>
+			<VocabularyTester allWords={allWords} language={language} />
+		</Container>
+	);
+};
+
+export default VocabularyTesterPage;
