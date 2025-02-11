ff6996e1355665afc10a7a4457bf04e271805727
Arseny Garelyshev <monstrag@gmail.com>
2025-02-11 21:22:01 +0100

    feat: drop dishes, didn't end up using those

--- src/app/(app)/dishes/dishes.ts
+++
@@ -1,8 +0,0 @@
-import { promises as fsPromises } from "fs";
-
-const dishesFile = "public/dishes.txt";
-
-export async function getAllDishes(): Promise<string[]> {
-	const content = await fsPromises.readFile(dishesFile, "utf8");
-	return content.split("\n");
-}
