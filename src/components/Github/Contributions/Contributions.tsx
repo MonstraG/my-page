3f22d5f7b5de0eee75e44c8b6abb1f836726c48e
Arseny Garelyshev <monstrag@gmail.com>
2025-02-12 18:12:50 +0100

    refactor: convert Tooltip to css modules

--- src/components/Github/Contributions/Contributions.tsx
+++ src/components/Github/Contributions/Contributions.tsx
@@ -1,8 +1,9 @@
 import type { FC } from "react";
 import { getContributions } from "@/components/Github/Contributions/getContributions";
 import { ContributionTable } from "@/components/Github/Contributions/ContributionTable";
-import { ContributionsTableTooltip } from "@/components/Github/Contributions/ContributionsTableTooltip";
 import styles from "./contributions.module.css";
+import { Tooltip } from "@/ui/Tooltip/Tooltip";
+import InfoIcon from "@mui/icons-material/Info";
 
 export const Contributions: FC = async () => {
 	const contributions = await getContributions();
@@ -11,7 +12,16 @@ export const Contributions: FC = async () => {
 		<>
 			<div className={styles.headerRow}>
 				<h2 className={styles.header}>Contributions table</h2>
-				<ContributionsTableTooltip />
+				<Tooltip
+					title={
+						<>
+							<p>Fetches data from github, re-implemented from scratch.</p>
+							<p>And maybe it should have been a table.</p>
+						</>
+					}
+				>
+					<InfoIcon />
+				</Tooltip>
 			</div>
 
 			<ContributionTable contributions={contributions} />
