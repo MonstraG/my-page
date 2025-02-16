c5f50094f33572bc2fd2ae1238fa5eaf0a713c51
Arseny Garelyshev <monstrag@gmail.com>
2025-02-16 20:44:18 +0100

    refactor: accordion as css start

--- src/components/words/StatCard.tsx
+++ src/components/words/StatCard.tsx
@@ -1,14 +1,9 @@
 import type { FC } from "react";
-import Box from "@mui/joy/Box";
 import { AnswerStats } from "@/components/words/AnswerStats";
-import CardOverflow from "@mui/joy/CardOverflow";
-import AccordionGroup from "@mui/joy/AccordionGroup";
-import Accordion from "@mui/joy/Accordion";
-import AccordionSummary from "@mui/joy/AccordionSummary";
-import AccordionDetails from "@mui/joy/AccordionDetails";
 import { AnswerMap } from "@/components/words/AnswerMap";
 import type { LanguageProgress } from "@/components/words/useWordsStore";
 import { BidirectionalCard } from "@/components/words/BidirectionalCard";
+import { Accordion } from "@/ui/Accordion/Accordion";
 
 interface Props {
 	progress: LanguageProgress;
@@ -19,32 +14,24 @@ export const StatCard: FC<Props> = ({ progress, totalWords }) => (
 	<BidirectionalCard
 		slots={{
 			left: (
-				<Box flexShrink={0} sx={{ p: 2, pb: { xs: 2, md: undefined } }}>
+				<div style={{ padding: "1rem" }}>
 					<AnswerStats known={progress.known.length} unknown={progress.unknown.length} />
-				</Box>
+				</div>
 			),
 			right: (
-				<CardOverflow sx={{ flexGrow: 1, p: 0 }}>
-					<AccordionGroup>
-						<Accordion>
-							<AccordionSummary>Answer map</AccordionSummary>
-							<AccordionDetails>
-								<AnswerMap
-									totalWords={totalWords}
-									known={progress.known}
-									unknown={progress.unknown}
-									invalid={progress.invalid}
-								/>
-							</AccordionDetails>
-						</Accordion>
-						<Accordion>
-							<AccordionSummary>Debug state</AccordionSummary>
-							<AccordionDetails>
-								<pre>{JSON.stringify(progress, null, 4)}</pre>
-							</AccordionDetails>
-						</Accordion>
-					</AccordionGroup>
-				</CardOverflow>
+				<>
+					<Accordion summary="Answer map">
+						<AnswerMap
+							totalWords={totalWords}
+							known={progress.known}
+							unknown={progress.unknown}
+							invalid={progress.invalid}
+						/>
+					</Accordion>
+					<Accordion summary="Debug state">
+						<pre>{JSON.stringify(progress, null, 4)}</pre>
+					</Accordion>
+				</>
 			)
 		}}
 	/>
