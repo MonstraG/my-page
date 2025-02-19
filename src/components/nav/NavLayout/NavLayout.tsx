17940c31c5c491ec942e9dfc0c4a36729a0daf4a
Arseny Garelyshev <monstrag@gmail.com>
2025-02-19 22:02:10 +0100

    feat: rewrite SpellList

--- src/components/nav/NavLayout/NavLayout.tsx
+++ src/components/nav/NavLayout/NavLayout.tsx
@@ -3,6 +3,7 @@ import styles from "./NavLayout.module.css";
 import { ListEndDecor } from "@/ui/ListEndDecor/ListEndDecor";
 import { Divider } from "@/ui/Divider/Divider";
 import { NavLinks } from "@/components/nav/NavLinks";
+import { Paragraph } from "@/ui/Paragraph/Paragraph";
 
 export const NavLayout: FCC = ({ children }) => (
 	<div className={styles.wrapper}>
@@ -13,9 +14,9 @@ export const NavLayout: FCC = ({ children }) => (
 
 			<ListEndDecor />
 
-			<span className={styles.version}>
+			<Paragraph size="sm" color="superGray" centered>
 				{process.env.NODE_ENV} {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.slice(0, 6)}
-			</span>
+			</Paragraph>
 		</aside>
 
 		<main className={styles.main}>{children}</main>
