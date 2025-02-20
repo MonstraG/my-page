cebc318ed88e1b1fdb865a39f0ef2fb59cb40802
Arseny Garelyshev <monstrag@gmail.com>
2025-02-20 18:11:31 +0100

    chore: just delete the theme

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,15 +1,14 @@
 import type { FCC } from "@/types/react";
-import { ThemeRegistry } from "@/components/theme/ThemeRegistry";
 import "@/ui/reset.css";
 import "@/ui/global.css";
 import { SnackbarHost } from "@/components/SnackbarHost";
 import { NavLayout } from "@/components/nav/NavLayout/NavLayout";
 
 const AppLayout: FCC = ({ children }) => (
-	<ThemeRegistry>
+	<>
 		<NavLayout>{children}</NavLayout>
 		<SnackbarHost />
-	</ThemeRegistry>
+	</>
 );
 
 export default AppLayout;
