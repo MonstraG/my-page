c64cb4b35c587586c739bbd64665a0a52b77a659
Arseny Garelyshev <monstrag@gmail.com>
2025-03-02 20:46:47 +0100

    refactor: rewrite chips

--- src/components/spells/ConcentrationChip.tsx
+++ src/components/spells/ConcentrationChip.tsx
@@ -1,12 +1,12 @@
 import type { FC } from "react";
-import Chip from "@mui/joy/Chip";
+import { Chip } from "@/ui/Chip/Chip";
 
 interface Props {
 	short?: boolean;
 }
 
 export const ConcentrationChip: FC<Props> = ({ short }) => (
-	<Chip sx={{ bgcolor: "rgba(89, 192, 184, 0.9)" }} variant="solid" size={short ? "sm" : "md"}>
+	<Chip style={{ background: "rgba(89, 192, 184, 0.9)" }} size={short ? "sm" : "md"}>
 		{short ? "K" : "Концентрация"}
 	</Chip>
 );
