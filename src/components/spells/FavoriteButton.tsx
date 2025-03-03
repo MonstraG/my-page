7748d1723bfc74e06cbdcdd28c365a6060800039
Arseny Garelyshev <monstrag@gmail.com>
2025-03-03 20:17:57 +0100

    fix: fix tooltips in distribution via hack

--- src/components/spells/FavoriteButton.tsx
+++ src/components/spells/FavoriteButton.tsx
@@ -3,21 +3,30 @@ import { Button } from "@/ui/Button/Button";
 import { type FavoriteSpellsActions } from "@/components/spells/favouriteSpellsStore";
 import { FavoriteIcon } from "@/icons/FavoriteIcon";
 import { FavoriteFilledIcon } from "@/icons/FavoriteFilledIcon";
+import { Tooltip, type TooltipProps } from "@/ui/Tooltip/Tooltip";
 
 interface Props {
 	spellId: number;
 	isFavorite: boolean;
 	toggleFavorite: FavoriteSpellsActions["toggleSpell"];
+	tooltipPlacement?: TooltipProps["placement"];
 }
 
-export const FavoriteButton: FC<Props> = ({ spellId, isFavorite, toggleFavorite }) => (
-	<Button
-		size="sm"
-		square
-		color="neutral"
-		variant="plain"
-		onClick={() => toggleFavorite(spellId, isFavorite)}
-	>
-		{isFavorite ? <FavoriteFilledIcon /> : <FavoriteIcon />}
-	</Button>
+export const FavoriteButton: FC<Props> = ({
+	spellId,
+	isFavorite,
+	toggleFavorite,
+	tooltipPlacement
+}) => (
+	<Tooltip title={isFavorite ? "Unfavorite" : "Favorite"} placement={tooltipPlacement}>
+		<Button
+			size="sm"
+			square
+			color="neutral"
+			variant="plain"
+			onClick={() => toggleFavorite(spellId, isFavorite)}
+		>
+			{isFavorite ? <FavoriteFilledIcon /> : <FavoriteIcon />}
+		</Button>
+	</Tooltip>
 );
--- src/ui/Tooltip/Tooltip.tsx
+++ src/ui/Tooltip/Tooltip.tsx
@@ -3,20 +3,22 @@ import styles from "./Tooltip.module.css";
 import type { HTMLAttributes, ReactNode } from "react";
 import { clsx } from "clsx";
 
-interface Props extends Omit<HTMLAttributes<HTMLDivElement>, "title"> {
+export interface TooltipProps extends Omit<HTMLAttributes<HTMLDivElement>, "title"> {
 	title: ReactNode;
 	arrow?: boolean;
 	disabled?: boolean;
 	open?: boolean;
+	placement?: "bottom" | "left";
 }
 
-export const Tooltip: FCC<Props> = ({
+export const Tooltip: FCC<TooltipProps> = ({
 	title,
 	arrow,
 	children,
 	disabled,
 	className,
 	open,
+	placement = "bottom",
 	...rest
 }) => (
 	<div
@@ -25,11 +27,20 @@ export const Tooltip: FCC<Props> = ({
 			!Boolean(disabled) && styles.enabled,
 			open == null && styles.uncontrolled,
 			open === true && styles.open,
+			placement === "left" && styles.placementLeft,
 			className
 		)}
 		{...rest}
 	>
 		{children}
-		<div className={clsx(styles.title, arrow && styles.arrow)}>{title}</div>
+		<div
+			className={clsx(
+				styles.title,
+				arrow && styles.arrow,
+				placement === "left" && styles.placementLeft
+			)}
+		>
+			{title}
+		</div>
 	</div>
 );
