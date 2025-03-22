c03730795c2c384633a5a80b5061d06d0ab4fbff
Arseny Garelyshev <monstrag@gmail.com>
2025-03-22 11:07:35 +0100

    feat(spells): move controls down closer to the list
    This allows to lift header section out of "use client"

--- src/app/(app)/dnd-spells/page.tsx
+++ src/app/(app)/dnd-spells/page.tsx
@@ -1,5 +1,7 @@
 import { DnDSpells } from "@/components/spells/DnDSpells";
+import { allSpells } from "@/components/spells/spellData/spells";
 import { Container } from "@/ui/Container/Container";
+import { Paragraph } from "@/ui/Paragraph/Paragraph";
 import { Stack } from "@/ui/Stack/Stack";
 import type { Metadata, NextPage } from "next";
 
@@ -9,7 +11,22 @@ export const metadata: Metadata = {
 
 const DndSpellsPage: NextPage = () => (
 	<Container>
-		<Stack gap={1}>
+		<Stack gap={2}>
+			<Stack direction="row" gap={1} style={{ justifyContent: "space-between" }}>
+				<Stack
+					direction="row"
+					gap={1}
+					style={{ alignItems: "end", flexWrap: "wrap", flexGrow: 1 }}
+				>
+					<h1>DnD spells</h1>
+				</Stack>
+			</Stack>
+
+			<Paragraph>
+				This includes all {allSpells.length}{" "}
+				spells available in free rules of 5.5e (2024) DnD.
+			</Paragraph>
+
 			<DnDSpells />
 		</Stack>
 	</Container>
