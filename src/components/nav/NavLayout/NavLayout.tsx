6af4baf95bebb06d8bacee7ce9c50e75aa9d0aaf
Arseny Garelyshev <monstrag@gmail.com>
2025-03-22 19:33:58 +0100

    fix: move footer to the bottom of the screen

--- src/components/nav/NavLayout/NavLayout.tsx
+++ src/components/nav/NavLayout/NavLayout.tsx
@@ -2,14 +2,15 @@ import { NavLinks } from "@/components/nav/NavLinks";
 import type { FCC } from "@/types/react";
 import { Divider } from "@/ui/Divider/Divider";
 import { Paragraph } from "@/ui/Paragraph/Paragraph";
+import styles from "./NavLayout.module.css";
 
 export const NavLayout: FCC = ({ children }) => (
-	<>
+	<div className={styles.wrapper}>
 		<header>
 			<NavLinks />
 			<Divider />
 		</header>
-		<main>
+		<main className={styles.main}>
 			{children}
 		</main>
 		<footer>
@@ -18,5 +19,5 @@ export const NavLayout: FCC = ({ children }) => (
 				{process.env.NODE_ENV} {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.slice(0, 6)}
 			</Paragraph>
 		</footer>
-	</>
+	</div>
 );
