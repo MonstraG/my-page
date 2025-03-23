67797add7b7a852f7e6f27e8ea6af070f9524ed2
Arseny Garelyshev <monstrag@gmail.com>
2025-03-23 20:55:54 +0100

    feat(spells): implement intermediate checkbox for "all classes"

--- src/ui/Checkbox/Checkbox.tsx
+++ src/ui/Checkbox/Checkbox.tsx
@@ -1,17 +1,18 @@
 import type { FCC } from "@/types/react";
-import { type ChangeEvent, type ReactElement } from "react";
+import { type ChangeEvent, type ReactElement, useCallback } from "react";
 import styles from "./Checkbox.module.css";
 
 export interface CheckboxProps {
 	checked: boolean;
 	onChange: (event: ChangeEvent<HTMLInputElement>) => void;
 	type: "checkbox" | "radio";
-
+	indeterminate?: boolean;
 	value?: string;
-	name?: string;
+	name: string;
 }
 
 export const Checkbox: FCC<CheckboxProps> = ({
+	indeterminate,
 	checked,
 	value,
 	onChange,
@@ -20,9 +21,21 @@ export const Checkbox: FCC<CheckboxProps> = ({
 	children,
 }): ReactElement => {
 	const inputId = `${name}-${value}`;
+
+	/**
+	 *  there is no prop; this is the only way to do it
+	 * 	https://github.com/facebook/react/issues/1798#issuecomment-417047897
+	 */
+	const setIndeterminateRef = useCallback((checkbox: HTMLInputElement) => {
+		if (checkbox) {
+			checkbox.indeterminate = Boolean(indeterminate);
+		}
+	}, [indeterminate]);
+
 	return (
 		<div key={inputId} className={styles.field}>
 			<input
+				ref={setIndeterminateRef}
 				className={styles.input}
 				type={type}
 				id={inputId}
