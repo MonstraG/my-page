cdc029dad3dc2ca5d53af6612dee7112d776388d
Arseny Garelyshev <monstrag@gmail.com>
2025-03-27 19:54:19 +0100

    refactor: create limited set of gap sizes

--- src/ui/Stack/Stack.tsx
+++ src/ui/Stack/Stack.tsx
@@ -3,26 +3,24 @@ import type { FC, HTMLAttributes } from "react";
 import styles from "./Stack.module.css";
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
-	gap?: number;
+	gap?: 0.25 | 0.5 | 1 | 2 | 3 | 4;
 	direction?: "row" | "column";
 	component?: "div" | "section";
 }
 
 export const Stack: FC<Props> = (
-	{ style, className, gap, direction, component = "div", ...props },
+	{ className, gap, direction, component = "div", ...props },
 ) => {
-	const resolvedProps = {
-		...props,
-		className: clsx(styles.stack, direction === "row" ? styles.row : styles.column, className),
-		style: {
-			...style,
-			gap: gap ? gap + "rem" : undefined,
-		},
-	};
+	const resolvedClassName = clsx(
+		styles.stack,
+		direction === "row" ? styles.row : styles.column,
+		gap && styles[`gap-${gap}`],
+		className,
+	);
 
 	if (component === "section") {
-		return <section {...resolvedProps} />;
+		return <section {...props} className={resolvedClassName} />;
 	}
 
-	return <div {...resolvedProps} />;
+	return <div {...props} className={resolvedClassName} />;
 };
