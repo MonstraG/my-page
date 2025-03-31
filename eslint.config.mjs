532b82bbe58ffc3a26062911ce6363c9d89c175b
Arseny Garelyshev <monstrag@gmail.com>
2025-03-31 21:06:50 +0200

    docs: cleanup eslint config a bit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-// @ts-expect-error There are no types https://github.com/vercel/next.js/issues/71864#issuecomment-2442249579
+import typescriptEslint from "typescript-eslint";
+// @ts-expect-error There are no types, should be fixed in next big next.js update
 import nextPlugin from "@next/eslint-plugin-next";
 import eslintPluginImportX from "eslint-plugin-import-x";
 import reactPlugin from "eslint-plugin-react";
@@ -44,15 +44,19 @@ const myConfig = {
 	name: "my config",
 	files: ["**/*.{mjs,ts,tsx}"],
 	rules: {
+		// avoid shipping a bunch of console.logs leftover accidentally, use other type to indicate it's important to keep
 		"no-console": ["error", { allow: ["debug", "warn", "error"] }],
 
 		// nobody actually wants those, and it's always a "forgot an import" type issue
 		"no-restricted-globals": ["error", "open", "close", "event"],
 
+		// require types to always be present
 		"@typescript-eslint/consistent-type-imports": [
 			"error",
 			{ fixStyle: "inline-type-imports" },
 		],
+
+		// allow unused vars for ignore or rest (that basically the only-non destructive way to remove a prop)
 		"@typescript-eslint/no-unused-vars": [
 			"error",
 			{
@@ -66,18 +70,14 @@ const myConfig = {
 		// explicit > implicit
 		"no-extra-boolean-cast": "off",
 
+		// sometimes we alias
 		"@typescript-eslint/no-empty-object-type": [
 			"error",
 			{ allowInterfaces: "with-single-extends" },
 		],
 
+		// improve type-check performance
 		"import-x/no-cycle": "error",
-		"@typescript-eslint/no-invalid-void-type": "off",
-		"@typescript-eslint/unified-signatures": [
-			"error",
-			{ ignoreDifferentlyNamedParameters: true },
-		],
-
 		"@typescript-eslint/explicit-module-boundary-types": "error",
 
 		// recommendations to disable rules by typescript-eslint
@@ -90,10 +90,10 @@ const myConfig = {
 	},
 };
 
-export default tseslint.config([
+export default typescriptEslint.config([
 	eslintConfig,
-	tseslint.configs.strict,
-	tseslint.configs.stylistic,
+	typescriptEslint.configs.strict,
+	typescriptEslint.configs.stylistic,
 	eslintPluginImportX.flatConfigs.recommended,
 	flatNextjsConfig,
 	myConfig,
