f60a2c46dd24e68557eb163192811d22d49fe3f9
Arseny Garelyshev <monstrag@gmail.com>
2025-04-07 19:47:02 +0200

    feat: move the onclick from the snack to the close button inside

--- src/ui/Snackbar/Snackbar.tsx
+++ src/ui/Snackbar/Snackbar.tsx
@@ -1,19 +1,27 @@
+import { CloseIcon } from "@/icons/material/CloseIcon";
 import type { FCC } from "@/types/react";
+import { Button } from "@/ui/Button/Button";
 import { Paragraph } from "@/ui/Paragraph/Paragraph";
 import { Sheet } from "@/ui/Sheet/Sheet";
+import { Stack } from "@/ui/Stack/Stack";
 import { clsx } from "clsx";
 import styles from "./Snackbar.module.css";
 
 interface Props {
 	open: boolean;
-	onClick: () => void;
+	handleCloseClick: () => void;
 	severity: "error" | "normal";
 }
 
-export const Snackbar: FCC<Props> = ({ open, onClick, severity, children }) => (
-	<div className={clsx(styles.host, open && styles.open)} onClick={onClick}>
+export const Snackbar: FCC<Props> = ({ open, handleCloseClick, severity, children }) => (
+	<div className={clsx(styles.host, open && styles.open)}>
 		<Sheet className={severity === "error" ? styles.error : undefined}>
-			<Paragraph size="sm">{children}</Paragraph>
+			<Stack direction="row" gap={1} style={{ alignItems: "start" }}>
+				<Paragraph size="sm" style={{ flexGrow: 1 }}>{children}</Paragraph>
+				<Button size="sm" square onClick={handleCloseClick}>
+					<CloseIcon />
+				</Button>
+			</Stack>
 		</Sheet>
 	</div>
 );
