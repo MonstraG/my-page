e05e19ae7bc61f72a61812a5d2ea9097c5b11bef
Arseny Garelyshev <monstrag@gmail.com>
2025-04-10 22:28:04 +0200

    feat: use built in encoder/decoder get to 19 min

--- src/app/(app)/tlm/page.tsx
+++ src/app/(app)/tlm/page.tsx
@@ -1,8 +1,11 @@
 import { ArticleContainer } from "@/ui/Container/ArticleContainer";
-import { promises as fsPromises } from "fs";
 import type { NextPage } from "next";
+import { promises as fsPromises } from "node:fs";
 
-// https://youtu.be/kCc8FmEb1nY?si=3BUykQtlayjkT_Jo&t=673
+const textEncoder = new TextEncoder();
+const textDecoder = new TextDecoder();
+
+// https://youtu.be/kCc8FmEb1nY?si=HbQAguqA5Tuo4rV2&t=1122
 
 const TinyLanguageModelPage: NextPage = async () => {
 	const tinyShakespeare = await fsPromises.readFile("public/tiny_shakespeare.txt", "utf-8");
@@ -12,6 +15,23 @@ const TinyLanguageModelPage: NextPage = async () => {
 	}
 	const alphabet = [...characters].sort();
 	const alphabetSize = alphabet.length;
+
+	const tensorShakespeare = textEncoder.encode(tinyShakespeare);
+
+	const trainSplitRatio = 0.9;
+	const trainSplitIndex = Math.floor(tensorShakespeare.length * trainSplitRatio);
+	const trainData = tensorShakespeare.slice(0, trainSplitIndex);
+	const verifyData = tensorShakespeare.slice(trainSplitIndex);
+
+	const blockSize = 8;
+
+	const x = trainData.slice(0, blockSize);
+	const y = trainData.slice(1, blockSize + 1);
+	for (let t = 0; t < blockSize; t++) {
+		const context = x.slice(0, t + 1);
+		const target = y[t];
+		console.log(`when input is ${context}, target is ${target}`);
+	}
 
 	return (
 		<ArticleContainer>
