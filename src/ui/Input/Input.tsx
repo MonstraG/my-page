abea2abd131898c8350852024b05d675f0d2466c
Arseny Garelyshev <monstrag@gmail.com>
2025-04-19 10:53:48 +0200

    feat(dice): add input to write dice notation, not click 10 times

--- src/ui/Input/Input.tsx
+++ src/ui/Input/Input.tsx
@@ -3,13 +3,16 @@ import type { FC, InputHTMLAttributes, ReactNode, RefObject } from "react";
 import styles from "./Input.module.css";
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
-	ref: RefObject<HTMLInputElement | null>;
+	ref?: RefObject<HTMLInputElement | null>;
 	startDecorator?: ReactNode;
 	endDecorator?: ReactNode;
+	invalid?: boolean;
 }
 
-export const Input: FC<Props> = ({ startDecorator, endDecorator, className, style, ...rest }) => (
-	<div className={clsx(styles.formControl, className)} style={style}>
+export const Input: FC<Props> = (
+	{ startDecorator, endDecorator, className, style, invalid, ...rest },
+) => (
+	<div className={clsx(styles.formControl, invalid && styles.invalid, className)} style={style}>
 		{startDecorator && <div className={styles.decorator}>{startDecorator}</div>}
 		<input className={styles.input} {...rest} />
 		{endDecorator && <div className={styles.decorator}>{endDecorator}</div>}
--- src/ui/MyLink/MyLink.tsx
+++ src/ui/MyLink/MyLink.tsx
@@ -1,7 +1,7 @@
 import type { FCC } from "@/types/react";
 import { clsx } from "clsx";
 import Link, { type LinkProps } from "next/link";
-import type { AnchorHTMLAttributes } from "react";
+import type { AnchorHTMLAttributes, HTMLAttributes } from "react";
 import styles from "./MyLink.module.css";
 
 interface MyLinkProps extends LinkProps, Omit<AnchorHTMLAttributes<HTMLAnchorElement>, "href"> {
@@ -14,6 +14,23 @@ export const MyLink: FCC<MyLinkProps> = ({ color, className, ...rest }) => (
 			styles.link,
 			color === "text-color" && styles.textColor,
 			color === "inherit" && styles.inheritColor,
+			className,
+		)}
+		{...rest}
+	/>
+);
+
+interface MyClickLinkProps extends HTMLAttributes<HTMLSpanElement> {
+	color?: "text-color" | "inherit";
+}
+
+export const MyClickLink: FCC<MyClickLinkProps> = ({ color, className, ...rest }) => (
+	<span
+		className={clsx(
+			styles.link,
+			color === "text-color" && styles.textColor,
+			color === "inherit" && styles.inheritColor,
+			styles.clickLink,
 			className,
 		)}
 		{...rest}
