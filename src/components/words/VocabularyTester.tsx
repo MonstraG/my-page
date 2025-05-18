ba5379509891c4b1e9b5dddfe522930af68dfc7f
Arseny Garelyshev <monstrag@gmail.com>
2025-05-18 21:08:06 +0200

    refactor: un-export some stuff from useWordsStore

--- src/components/words/VocabularyTester.tsx
+++ src/components/words/VocabularyTester.tsx
@@ -1,11 +1,7 @@
 "use client";
 import { MainControls } from "@/components/words/MainControls";
 import { StatCard } from "@/components/words/StatCard";
-import {
-	emptyLanguageProgress,
-	type Language,
-	useWordsStore,
-} from "@/components/words/useWordsStore";
+import { type Language, useWordsStoreForLanguage } from "@/components/words/useWordsStore";
 import { MyLink } from "@/ui/MyLink/MyLink";
 import { Paragraph } from "@/ui/Paragraph/Paragraph";
 import { Stack } from "@/ui/Stack/Stack";
@@ -17,8 +13,7 @@ interface Props {
 }
 
 export const VocabularyTester: FC<Props> = ({ allWords, language }) => {
-	const wordState = useWordsStore();
-	const progress = wordState.progress[language] ?? emptyLanguageProgress;
+	const progress = useWordsStoreForLanguage(language);
 
 	const currentWord = allWords[progress.currentIndex];
 
