7d6bb5ed789804c8b26b38ed4cabc0a8fdc37c6f
Arseny Garelyshev <monstrag@gmail.com>
2025-11-10 20:08:41 +0100

    refactor: get rid of lint rule spam

--- src/ui/Dialog/Dialog.tsx
+++ src/ui/Dialog/Dialog.tsx
@@ -7,32 +7,29 @@ interface Props {
 	close: () => void;
 }
 
-export const Dialog: FCC<Props> = ({ isOpen, close, children }) => {
-	return (
-		// biome-ignore lint/a11y/noNoninteractiveElementInteractions: https://github.com/biomejs/biome/issues/7783
-		<dialog
-			className={styles.dialog}
-			ref={(dialog) => {
-				if (!dialog) {
-					return;
-				}
+export const Dialog: FCC<Props> = ({ isOpen, close, children }) => (
+	<dialog
+		className={styles.dialog}
+		ref={(dialog) => {
+			if (!dialog) {
+				return;
+			}
 
-				if (isOpen && !dialog.open) {
-					dialog.showModal();
-				}
-				if (!isOpen && dialog.open) {
-					dialog.close();
-				}
-			}}
-			onKeyDown={(event) => {
-				// don't close it trough html, only notify control
-				event.preventDefault();
-				if (event.code === "Escape") {
-					close();
-				}
-			}}
-		>
-			<Sheet>{children}</Sheet>
-		</dialog>
-	);
-};
+			if (isOpen && !dialog.open) {
+				dialog.showModal();
+			}
+			if (!isOpen && dialog.open) {
+				dialog.close();
+			}
+		}}
+		onKeyDown={(event) => {
+			// don't close it trough html, only notify control
+			event.preventDefault();
+			if (event.code === "Escape") {
+				close();
+			}
+		}}
+	>
+		<Sheet>{children}</Sheet>
+	</dialog>
+);
