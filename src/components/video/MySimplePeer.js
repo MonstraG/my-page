7d71cdd143b7b2f5021b55365f48b8786f1a1e09
Arseny Garelyshev <monstrag@gmail.com>
2026-01-12 21:04:25 +0100

    refactor: remove "buffer" usage from `randomBytes`

--- src/components/video/MySimplePeer.js
+++ src/components/video/MySimplePeer.js
@@ -6,42 +6,8 @@
 import stream from "readable-stream";
 import { Buffer } from "buffer";
 
-function randomBytes(size, cb) {
-	// limit of Crypto.getRandomValues()
-	// https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues
-	const MAX_BYTES = 65536;
-
-	// Node supports requesting up to this number of bytes
-	// https://github.com/nodejs/node/blob/master/lib/internal/crypto/random.js#L48
-	const MAX_UINT32 = 4294967295;
-
-	// phantomjs needs to throw
-	if (size > MAX_UINT32) throw new RangeError("requested too many random bytes");
-
-	const bytes = Buffer.allocUnsafe(size);
-
-	if (size > 0) {
-		// getRandomValues fails on IE if size == 0
-		if (size > MAX_BYTES) {
-			// this is the max bytes crypto.getRandomValues
-			// can do at once see https://developer.mozilla.org/en-US/docs/Web/API/window.crypto.getRandomValues
-			for (let generated = 0; generated < size; generated += MAX_BYTES) {
-				// buffer.slice automatically checks if the end is past the end of
-				// the buffer so we don't have to here
-				crypto.getRandomValues(bytes.slice(generated, generated + MAX_BYTES));
-			}
-		} else {
-			crypto.getRandomValues(bytes);
-		}
-	}
-
-	if (typeof cb === "function") {
-		return process.nextTick(() => {
-			cb(null, bytes);
-		});
-	}
-
-	return bytes;
+function randomBytes(size) {
+	return crypto.getRandomValues(new Uint8Array(size));
 }
 
 function getBrowserRTC() {
@@ -81,12 +47,10 @@ class MySimplePeer extends stream.Duplex {
 
 		super(opts);
 
-		this._id = randomBytes(4).toString("hex").slice(0, 7);
+		this._id = randomBytes(4).toHex().slice(0, 7);
 		this._debug("new peer %o", opts);
 
-		this.channelName = opts.initiator
-			? opts.channelName || randomBytes(20).toString("hex")
-			: null;
+		this.channelName = opts.initiator ? opts.channelName || randomBytes(20).toHex() : null;
 
 		this.initiator = opts.initiator || false;
 		this.channelConfig = opts.channelConfig || MySimplePeer.channelConfig;
