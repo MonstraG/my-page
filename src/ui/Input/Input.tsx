f396c8fdfd7f17d1efc9489affb96631951b0186
Arseny Garelyshev <monstrag@gmail.com>
2026-01-27 19:06:59 +0100

    refactor: allow clicking trough the start/end decorators on inputs

--- src/ui/Input/Input.tsx
+++ src/ui/Input/Input.tsx
@@ -2,14 +2,14 @@ import { cn } from "@/functions/cn";
 import type { FC, InputHTMLAttributes, ReactNode, RefObject } from "react";
 import styles from "./Input.module.css";
 
-interface Props extends InputHTMLAttributes<HTMLInputElement> {
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 	ref?: RefObject<HTMLInputElement | null>;
 	startDecorator?: ReactNode;
 	endDecorator?: ReactNode;
 	invalid?: boolean;
 }
 
-export const Input: FC<Props> = ({
+export const Input: FC<InputProps> = ({
 	startDecorator,
 	endDecorator,
 	className,
@@ -19,7 +19,14 @@ export const Input: FC<Props> = ({
 }) => (
 	<div className={cn(styles.formControl, invalid && styles.invalid, className)} style={style}>
 		{startDecorator && <div className={styles.decorator}>{startDecorator}</div>}
-		<input className={styles.input} {...rest} />
+		<input
+			className={cn(
+				styles.input,
+				startDecorator && styles.hasStartDecorator,
+				endDecorator && styles.hasEndDecorator,
+			)}
+			{...rest}
+		/>
 		{endDecorator && <div className={styles.decorator}>{endDecorator}</div>}
 	</div>
 );
